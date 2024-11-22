2c39aa09cf17f8bf218bcbfc27370c7734d208df
carine.lima <caricalima1@gmail.com>
2024-11-22 11:47:55 -0300

    delete: inserido na pasta core

--- src/entities/TaskStatus.ts
+++
@@ -1,6 +0,0 @@
-export enum TaskStatus {
-  OPEN = "OPEN",
-  IN_PROGRESS = "IN_PROGRESS",
-  UNDER_REVIEW = "UNDER_REVIEW",
-  DONE = "DONE",
-}
--- src/entities/TaskType.ts
+++
@@ -1,6 +0,0 @@
-export enum TaskType {
-  BUG = 'BUG',
-  TASK = 'TASK',
-  SUB_TASK = 'SUB_TASK',
-  EPIC = 'EPIC',
-}
