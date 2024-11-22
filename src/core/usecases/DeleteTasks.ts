9233428b704be46a66a8e4405e2761bafcf3abb0
carine.lima <caricalima1@gmail.com>
2024-11-22 11:39:35 -0300

    chore: Caso de uso deletar tarefas

---
+++ src/core/usecases/DeleteTasks.ts
@@ -0,0 +1,22 @@
+import { TaskStatus } from "../entities/TaskStatus";
+import { InvalidOperationException } from "../exceptions/InvalidOperationException";
+import { ResourceNotFoundException } from "../exceptions/ResourceNotFoundException";
+import { TaskRepository } from "../repositores/TaskRepository";
+
+export class DeleteTask {
+  constructor(private readonly repository: TaskRepository) {}
+
+  public async execute(id: string) {
+    const task = await this.repository.findById(id);
+
+    if (!task) {
+      throw new ResourceNotFoundException();
+    }
+
+    if (task.getStatus() === TaskStatus.DONE) {
+      throw new InvalidOperationException();
+    }
+
+    await this.repository.delete(id);
+  }
+}
